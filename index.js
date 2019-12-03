4ff0e8af75f8e519fc010fdf6ac4186e5c74e1b6
Tania Manco <belalov.tania16@gmail.com>
2019-12-03 23:31:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Add your functions here
+function map(arr, fun) {
+  let newArr = [];
+  
+  for(let i = 0; i < arr.length; i++){
+    let newEl = arr[i]; 
+    newArr.push(fun(newEl));
+  }
+  return newArr; 
+}
