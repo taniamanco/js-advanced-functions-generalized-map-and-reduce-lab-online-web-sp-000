eeabc1ba94945b105178f86645c26c4b842160af
Tania Manco <belalov.tania16@gmail.com>
2019-12-04 03:15:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Add your functions here
+function map(arr, callback) {
+  let newArr = [];
+  
+  for(let i = 0; i < arr.length; i++) {
+    let newEl = arr[i]; 
+    newArr.push(callback(newEl));
+  }
+  return newArr; 
+}
+
+function reduce(arr, callback, startPoint) {
+  let memo = (!!startPoint) ? startPoint : arr[0];
+  let i = (!!startPoint) ? 0 : 1
+  for(; i < arr.length; i++) {
+      memo = callback(arr[i], memo);
+  }
+  return memo;
+}
